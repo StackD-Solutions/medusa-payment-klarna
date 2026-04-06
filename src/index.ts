d86a93a6cdf47e25b4a6ea19c52671aa44caf56e
A.G.J. Cate <brummos@gmail.com>
2026-04-06 16:14:30 +0200

    fix: export ModuleProvider as default

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import {ModuleProvider, Modules} from '@medusajs/framework/utils'
+import KlarnaPaymentService from './modules/payment-klarna/service'
+
 export type {KlarnaOptions, KlarnaLineItemInput, KlarnaSessionData, KlarnaPaymentData} from './modules/payment-klarna/service'
 export {KlarnaApiError, KlarnaFraudStatus, KlarnaOrderStatus, KlarnaHppSessionStatus, KlarnaWebhookEvent} from './modules/payment-klarna/client'
 export type {KlarnaOrderLine, KlarnaPaymentMethodCategory, KlarnaOrderResponse, KlarnaOrderDetails} from './modules/payment-klarna/client'
+
+export default ModuleProvider(Modules.PAYMENT, {
+  services: [KlarnaPaymentService],
+})
